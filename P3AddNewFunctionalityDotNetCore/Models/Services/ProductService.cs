01021fa924e0783c52da3680a97ac622675e0968
Gerald Chifanzwa <gchifianzwa@gmail.com>
2021-05-30 12:15:21 +0200

    Implemented Fluent validation for the validation of ProductViewModel

--- P3AddNewFunctionalityDotNetCore/Models/Services/ProductService.cs
+++ P3AddNewFunctionalityDotNetCore/Models/Services/ProductService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Localization;
 using P3AddNewFunctionalityDotNetCore.Models.Entities;
 using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Validation;
 using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.Services
@@ -89,47 +90,12 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
                 _productRepository.UpdateProductStocks(line.Product.Id, line.Quantity);
             }
         }
-
-        // TODO this is an example method, remove it for 
+        
         public List<string> CheckProductModelErrors(ProductViewModel product)
         {
-            List<string> modelErrors = new List<string>();
-            if (product.Name == null || string.IsNullOrWhiteSpace(product.Name))
-            {
-                modelErrors.Add(_localizer["MissingName"]);
-            }
-
-            if (product.Price == null || string.IsNullOrWhiteSpace(product.Price))
-            {
-                modelErrors.Add(_localizer["MissingPrice"]);
-            }
-
-            if (!Double.TryParse(product.Price, out double pc))
-            {
-                modelErrors.Add(_localizer["PriceNotANumber"]);
-            }
-            else
-            {
-                if (pc <= 0)
-                    modelErrors.Add(_localizer["PriceNotGreaterThanZero"]);
-            }
-
-            if (product.Stock == null || string.IsNullOrWhiteSpace(product.Stock))
-            {
-                modelErrors.Add(_localizer["MissingQuantity"]);
-            }
-
-            if (!int.TryParse(product.Stock, out int qt))
-            {
-                modelErrors.Add(_localizer["StockNotAnInteger"]);
-            }
-            else
-            {
-                if (qt <= 0)
-                    modelErrors.Add(_localizer["StockNotGreaterThanZero"]);
-            }
-
-            return modelErrors;
+            var validator = new ProductViewModelValidator(_localizer);
+            var result = validator.Validate(product);
+            return result.Errors.Select(error => error.ErrorMessage).ToList();
         }
 
         public void SaveProduct(ProductViewModel product)
---
+++ P3AddNewFunctionalityDotNetCore/Models/Validation/ProductViewModelValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Localization;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+
+namespace P3AddNewFunctionalityDotNetCore.Models.Validation
+{
+    public class ProductViewModelValidator : AbstractValidator<ProductViewModel>
+    {
+        public ProductViewModelValidator(IStringLocalizer localizer)
+        {
+            RuleFor(p => p.Name)
+               .NotEmpty()
+               .WithMessage(localizer["MissingName"]);
+
+            RuleFor(p => p.Price)
+               .Cascade(CascadeMode.Stop)
+               .NotEmpty().WithMessage(_ => localizer["MissingPrice"])
+               .Must(value => int.TryParse(value, out _)).WithMessage(_ => localizer["PriceNotANumber"])
+               .Must(value => int.Parse(value) > 0).WithMessage(_ => localizer["PriceNotGreaterThanZero"]);
+
+            RuleFor(p => p.Stock)
+               .Cascade(CascadeMode.Stop)
+               .NotEmpty().WithMessage(_ => localizer["MissingQuantity"])
+               .Must(value => int.TryParse(value, out _)).WithMessage(_ => localizer["StockNotAnInteger"])
+               .Must(value => int.Parse(value) > 0).WithMessage(_ => localizer["StockNotGreaterThanZero"]);
+        }
+    }
+}
