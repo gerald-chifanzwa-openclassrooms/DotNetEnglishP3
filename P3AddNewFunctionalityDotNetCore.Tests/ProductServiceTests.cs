f135183f271137b4f5f26b21411b8d665193559a
Gerald Chifanzwa <gchifianzwa@gmail.com>
2021-05-30 11:47:53 +0200

    Added Unit tests to test the validation logic in CheckProductModelErrors against requirements

--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -1,26 +1,202 @@
-﻿using Xunit;
+﻿using Bogus;
+using FluentAssertions;
+using Microsoft.Extensions.Localization;
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using Xunit;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
-        /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
-        /// </summary>
-        [Fact]
-        public void ExampleMethod()
+        private readonly Mock<ICart> _cartMock;
+        private readonly Mock<IProductRepository> _productRepositoryMock;
+        private readonly Mock<IOrderRepository> _orderRepositoryMock;
+        private readonly Mock<IStringLocalizer<ProductService>> _localizerMock;
+
+        public ProductServiceTests()
         {
-            // Arrange
+            _cartMock = new Mock<ICart>();
+            _productRepositoryMock = new Mock<IProductRepository>();
+            _orderRepositoryMock = new Mock<IOrderRepository>();
+            _localizerMock = new Mock<IStringLocalizer<ProductService>>(MockBehavior.Loose);
 
-            // Act
-
-
-            // Assert
-            Assert.Equal(1, 1);
+            _localizerMock.Setup(localizer => localizer[It.IsAny<string>()])
+                          .Returns((string name) => new LocalizedString(name, name, true));
         }
 
-        // TODO write test methods to ensure a correct coverage of all possibilities
+        private ProductService GetProductsService()
+        {
+            var cartMock = _cartMock.Object;
+            var productRepositoryMock = _productRepositoryMock.Object;
+            var orderRepositoryMock = _orderRepositoryMock.Object;
+            var localizerMock = _localizerMock.Object;
+
+            return new ProductService(cartMock, productRepositoryMock, orderRepositoryMock, localizerMock);
+        }
+
+        [Fact]
+        public void CheckProductErrors_WhenNameIsEmpty_ShouldHaveMissingNameError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, string.Empty)
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Price, f => f.Commerce.Price())
+                       .RuleFor(x => x.Stock, f => f.Random.Int(1, 200).ToString())
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("MissingName");
+        }
+
+        [Fact]
+        public void CheckProductErrors_WhenPriceIsEmpty_ShouldHaveMissingPriceError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductMaterial())
+                       .RuleFor(x => x.Price, string.Empty)
+                       .RuleFor(x => x.Stock, f => f.Random.Int(1, 200).ToString())
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("MissingPrice");
+        }
+
+        [Fact]
+        public void CheckProductErrors_WhenPriceIsNotANumber_ShouldHavePriceNotANumberError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductMaterial())
+                       .RuleFor(x => x.Price, f => f.Random.String2(1, 5))
+                       .RuleFor(x => x.Stock, f => f.Random.Int(1, 200).ToString())
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("PriceNotANumber");
+        }
+
+        [Fact]
+        public void CheckProductErrors_WhenPriceIsNotGreaterThanZero_ShouldHavePriceNotGreaterThanZeroError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductMaterial())
+                       .RuleFor(x => x.Price, "0")
+                       .RuleFor(x => x.Stock, f => f.Random.Int(1, 200).ToString())
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("PriceNotGreaterThanZero");
+        }
+        
+        [Fact]
+        public void CheckProductErrors_WhenStockIsEmpty_ShouldHaveMissingQuantityError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductMaterial())
+                       .RuleFor(x => x.Price, f => f.Commerce.Price())
+                       .RuleFor(x => x.Stock, string.Empty)
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("MissingQuantity");
+        }
+
+        [Fact]
+        public void CheckProductErrors_WhenStockIsNotANumber_ShouldHaveStockNotAnIntegerError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductMaterial())
+                       .RuleFor(x => x.Price, f => f.Commerce.Price())
+                       .RuleFor(x => x.Stock, f => f.Random.String2(1, 5))
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("StockNotAnInteger");
+        }
+
+        [Fact]
+        public void CheckProductErrors_WhenStockIsNotGreaterThanZero_ShouldHaveStockNotGreaterThanZeroError()
+        {
+            // Arrange
+            var faker = new Faker<ProductViewModel>()
+                       .RuleFor(x => x.Name, f => f.Commerce.ProductName())
+                       .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                       .RuleFor(x => x.Details, f => f.Commerce.ProductMaterial())
+                       .RuleFor(x => x.Price, f=>f.Commerce.Price())
+                       .RuleFor(x => x.Stock, "0")
+                       .RuleFor(x => x.Id, 0);
+
+            var productViewModel = faker.Generate();
+            var testSubject = GetProductsService();
+
+            // Act
+            var errors = testSubject.CheckProductModelErrors(productViewModel);
+
+            // Assert
+            errors.Should().NotBeEmpty();
+            errors.Should().Contain("StockNotGreaterThanZero");
+        }
     }
 }
